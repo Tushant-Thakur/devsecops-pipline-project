bec4f61fb7712c74167c06b161435d2d97258338
Tushant Pratap Singh <tushantthakur1962006@gmail.com>
2025-08-31 21:05:34 +0530

    Update CalculatorTest.java

--- src/src/test/java/com/example/CalculatorTest.java
+++ src/src/test/java/com/example/CalculatorTest.java
@@ -5,27 +5,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    Calculator calc = new Calculator();
+
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
+        assertEquals(-1, calc.add(-2, 1));
+        assertEquals(0, calc.add(0, 0));
     }
 
     @Test
     public void testSubtract() {
-        Calculator calc = new Calculator();
         assertEquals(1, calc.subtract(3, 2));
+        assertEquals(-3, calc.subtract(-2, 1));
+        assertEquals(0, calc.subtract(2, 2));
     }
 
     @Test
     public void testMultiply() {
-        Calculator calc = new Calculator();
         assertEquals(6, calc.multiply(2, 3));
+        assertEquals(-6, calc.multiply(-2, 3));
+        assertEquals(0, calc.multiply(2, 0));
     }
 
     @Test
     public void testDivide() {
-        Calculator calc = new Calculator();
         assertEquals(2, calc.divide(6, 3));
+        assertEquals(-2, calc.divide(-6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calc.divide(5, 0);
+        });
+        assertEquals("Division by zero is not allowed!", exception.getMessage());
     }
 }
