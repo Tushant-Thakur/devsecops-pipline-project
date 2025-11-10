bbc54edd6e5b8fdfd9ef117aacc8e5abc164e249
Tushant Pratap Singh <tushantthakur1962006@gmail.com>
2025-11-10 09:44:00 +0530

    Delete src/main/java/com/example/calculator/TestVuln.java

--- src/main/java/com/example/calculator/TestVuln.java
+++
@@ -1,6 +0,0 @@
-public class TestVuln {
-    public static void main(String[] args) throws Exception {
-        // 🚨 Security Risk Example — for testing only
-        Runtime.getRuntime().exec("ls"); // Sonar will flag this as a security hotspot
-    }
-}
