f1628ec176eeb9d1a4c6807dd106f9bc7a408e70
Tushant Pratap Singh <tushantthakur1962006@gmail.com>
2025-11-25 18:35:55 +0530

    pom updated

--- src/src/test/java/com/example/CalculatorTest.java
+++ src/src/test/java/com/example/CalculatorTest.java
@@ -1,8 +1,9 @@
 package com.example.calculator;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
