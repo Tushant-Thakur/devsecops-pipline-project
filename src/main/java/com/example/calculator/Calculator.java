6afbc8fa296a9eb3403213ccdf4cad3d87e63ba7
Tushant Pratap Singh <tushantthakur1962006@gmail.com>
2026-01-15 18:13:59 +0530

    Uncomment divide method in Calculator class

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -13,12 +13,12 @@ public class Calculator {
         return a * b;
     }
 
-   /* public int divide(int a, int b) {
+    public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed!");
         }
         return a / b;
-    }*/
+    }
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
